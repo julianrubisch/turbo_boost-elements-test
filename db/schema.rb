a16450712cc6ab4400f8da64537d2becfc58f38d
Julian Rubisch <julian@julianrubisch.at>
2023-07-17 14:07:03 +0200

    chore: Add section scaffold to test expand/collapse all

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_102022) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_190528) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_102022) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sections", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_sections_on_post_id"
+  end
+
+  add_foreign_key "sections", "posts"
 end
